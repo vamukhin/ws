7e9be49db8959c7aae0941fd153f74a6363201ec
mkhnvld <va.mukhin@physics.msu.ru>
2019-08-24 15:16:48 +0300

    express added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 var WebSocketServer = new require('ws');
+const express = require('express');
+const path = require('path');
+
 import {onMessageHandler} from "./events/on-message"
 
+const PORT = 8081;
+const INDEX = path.join(__dirname, 'index.html');
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 let webSocketServer = new WebSocketServer.Server({
-  port: 8081
+  server
 });
 
 global.clients = webSocketServer.clients;
